2d27fc51d8a7895d563ccccd93223522c9bf03a5
DcWind <danielekwuazi@gmail.com>
2016-11-02 23:45:57 +0700

    finished header, began app js

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+
+
+// action when button is clicked on
+
+  document.getElementById('addItem').addEventListener('click', function () {
+    var value = document.getElementById('item').value;
+    if(value){
+      console.log(value);
+    }
+  });

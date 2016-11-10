3a6b22687572199fcb2e477b0c9c0ec873c13951
DcWind <danielekwuazi@gmail.com>
2016-11-10 23:02:57 +0700

    js .classList.add

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,19 @@
 
   document.getElementById('addItem').addEventListener('click', function () {
     var value = document.getElementById('item').value;
-    if(value){
+    if(value) addItemTodo(value);
 
-    }
   });
 
-function add
+function addItemTodo (text) {
+  var item = document.createElement('li');
+  item.innerText = text;
+
+  var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+
+  var remove = document.createElement('button');
+  remove.classList.add('remove');
+  remove.innerHTML = removeSVG;
+
+}

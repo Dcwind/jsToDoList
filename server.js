595abadd1f412b9c17f9050bcec5bd9de8f881f1
DcWind <danielekwuazi@gmail.com>
2016-11-19 23:56:22 +0700

    ajax script- response from server

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+var task = [
+  {
+    id: 1,
+    name: 'first phase project',
+  },
+  {
+    id: 2,
+    name: 'microwave'
+  }
+];
+
+var currentId = 2;
+
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
